6ddaef8dd85ed12166896ecea4f9b085a3b35cfe
hukaiems <nguoinhen0105@gmail.com>
2025-11-21 12:30:50 +0700

    added 10 subset csv

--- models/TSViT/TSViTdense.py
+++ models/TSViT/TSViTdense.py
@@ -420,7 +420,7 @@ class TSViT(nn.Module):
         xt = xt.reshape(-1, 366)
         temporal_pos_embedding = self.to_temporal_embedding_input(xt).reshape(B, T, self.dim) # learn unique vector for the date
         x = self.to_patch_embedding(x)
-        x = x.reshape(B, -1, T, self.dim)
+        x = x.reshape(B, -1, T, self.dim) # Shape: B, 
         x += temporal_pos_embedding.unsqueeze(1)
         x = x.reshape(-1, T, self.dim)
         cls_temporal_tokens = repeat(self.temporal_token, '() N d -> b N d', b=B * self.num_patches_1d ** 2)
