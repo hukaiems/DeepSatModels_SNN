99e324adcc3d397aaf0a95fad40c3c59b9300d20
hukaiems <nguoinhen0105@gmail.com>
2025-12-21 08:36:30 +0700

    fix code typo confusion matrix

--- train_and_eval/inference_s_tsvit.py
+++ train_and_eval/inference_s_tsvit.py
@@ -88,9 +88,9 @@ def compute_and_plot_cm(model, val_loader, device, num_classes=20, class_names=N
     with torch.no_grad():
         for batch in tqdm(val_loader, desc="Inferencing"):
             # 1. Unpack Batch (Match this to your specific keys)
-            inputs = batch['image'].to(device)
-            dates = batch['dates'].to(device)
-            targets = batch['mask'].to(device)
+            inputs = batch['img'].to(device)
+            dates = batch['doy'].to(device)
+            targets = batch['labels'].to(device)
             
             # 2. Forward Pass
             outputs = model(inputs, dates)
