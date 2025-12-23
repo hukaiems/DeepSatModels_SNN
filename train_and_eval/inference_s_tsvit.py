7cdfd62e32590cbff53d1dd1ef6e82205a355d9c
hukaiems <nguoinhen0105@gmail.com>
2025-12-23 09:54:44 +0700

    added debugging mode into inference file

--- train_and_eval/inference_s_tsvit.py
+++ train_and_eval/inference_s_tsvit.py
@@ -24,9 +24,10 @@ import pandas as pd
 from torchmetrics.classification import MulticlassJaccardIndex # mIoU score
 
 from spike_data.pastis_dataset import PastisDataset
+from spike_data.france_dataset import FranceDataset
 from models.snn.spike_tsvit import SpikeTSViTMean, SpikeTSViTNoMean
 from spikingjelly.clock_driven.functional import reset_net
-from models.snn.helper_functions import measure_energy_efficiency_full
+from models.snn.helper_functions import measure_energy_efficiency_full, check_class_imbalance
 
 # --- ARGUMENT PARSER ---
 def get_args():
@@ -51,6 +52,7 @@ def get_args():
     parser.add_argument('--norm_type', type=str, default='gn', 
                         choices=['bn', 'gn'],
                         help="Normalization layer: 'bn' (Batch Norm) or 'gn' (Group Norm / Layer Norm)")
+    parser.add_argument('--datasets', type=str, default='pastis', choices=['pastis', 'france'], help='choose the dataset')
 
     # Attention Mode
     parser.add_argument('--att_mode', type=str, default='2D_dot', choices=['2D_dot', '2D_ham'], 
@@ -58,8 +60,15 @@ def get_args():
 
     parser.add_argument('--num_workers', type=int, default=4,
                         help="Number of CPU processors to load data for the model")
+    # Testing type
     parser.add_argument('--no_progress_bar', action='store_true', 
                         help="Disable tqdm progress bar (useful for Kaggle Commit/Save Version)")
+    parser.add_argument('--inference', action='store_true',
+                        help='Run inferencing for the checkpoint')
+    parser.add_argument('--test_per_class', action='store_true',
+                        help="Test the per class mIoU")
+    parser.add_argument('--test_energy', action='store_true',
+                        help="Test the energy efficiency of the model")
 
     return parser.parse_args()
 
@@ -69,7 +78,7 @@ def main():
     args = get_args()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    print(f"--- 🚀 Starting Training ---")
+    print(f"--- 🚀 Starting Inferencing ---")
     print(f"📋 EXPERIMENT CONFIGURATION")
     print(f"{'='*40}")
     
@@ -81,6 +90,19 @@ def main():
     # Hyperparameters
     print(f"\n⚙️  Hyperparameters:")
     print(f"   Batch Size:      {args.batch_size}")
+
+    if args.datasets == 'france':
+        DatasetLoader = FranceDataset
+        in_channels = 13
+        num_classes = 21
+        ignore_index = 20
+        print(f"Dataset Selected: France dataset.")
+    else: 
+        DatasetLoader = PastisDataset
+        in_channels = 10
+        num_classes = 20
+        ignore_index = 19
+        print(f"Dataset Selected: PASTIS dataset.")
 
     # Model Architecture
     print(f"\n🧠 Model Architecture:")
@@ -116,15 +138,15 @@ def main():
     val_df = pd.read_csv(args.val_csv_path, header=None)
 
     val_loader = DataLoader(
-        PastisDataset(val_df, args.data_root, max_seq_len=args.max_seq_len, mode='eval'),
+        DatasetLoader(val_df, args.data_root, max_seq_len=args.max_seq_len, mode='eval'),
         batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers, drop_last=True
     )
 
     # 2. Model Setup
     model = ModelType(
-        in_channels=10,
+        in_channels=in_channels,
         embed_dim=args.embed_dim,
-        num_classes=20,
+        num_classes=num_classes,
         spatial_depth=args.spatial_depth,
         temporal_depth=args.temporal_depth,
         att_mode=args.att_mode,
@@ -148,39 +170,44 @@ def main():
     model.eval()
 
     # ---------------------------------------------------------
-    # ⚡ PHASE 1: Energy Efficiency Analysis
+    # Per class mIoU Analysis
     # ---------------------------------------------------------
-    # This runs BEFORE the accuracy loop. It pushes data through,
-    # counts spikes, and prints the "15x Efficiency" stat.
-    # ---------------------------------------------------------
-    measure_energy_efficiency_full(model, val_loader, device, disable_tqdm=args.no_progress_bar)
+    if args.test_per_class:
+        check_class_imbalance(model, val_loader, device=device, num_classes=num_classes)
 
     # ---------------------------------------------------------
-    # 🎯 PHASE 2: Accuracy Evaluation (mIoU)
+    # Energy Efficiency Analysis
+    # ---------------------------------------------------------
+    if args.test_energy:
+        measure_energy_efficiency_full(model, val_loader, device, disable_tqdm=args.no_progress_bar)
+
+    # ---------------------------------------------------------
+    # Accuracy Evaluation (mIoU)
     # ---------------------------------------------------------
     print("\n--- 🎯 Starting Accuracy Evaluation ---")
-    metric = MulticlassJaccardIndex(num_classes=20, average='macro').to(device)
+    metric = MulticlassJaccardIndex(num_classes=num_classes, average='macro', ignore_index=ignore_index).to(device)
     
-    with torch.no_grad():
-        for batch in tqdm(val_loader, desc="Evaluating Accuracy", disable=args.no_progress_bar):
-            x = batch['sequence'].to(device)
-            dates = batch['dates'].to(device)
-            y = batch['labels'].to(device)
+    if args.inference:
+        with torch.no_grad():
+            for batch in tqdm(val_loader, desc="Evaluating Accuracy", disable=args.no_progress_bar):
+                x = batch['sequence'].to(device)
+                dates = batch['dates'].to(device)
+                y = batch['labels'].to(device)
 
-            # Inference
-            logits = model(x, dates)
-            preds = torch.argmax(logits, dim=1)
-            
-            # Update Metric
-            metric.update(preds, y)
-            
-            # Reset SNN states (Voltage = 0)
-            reset_net(model)
+                # Inference
+                logits = model(x, dates)
+                preds = torch.argmax(logits, dim=1)
+                
+                # Update Metric
+                metric.update(preds, y)
+                
+                # Reset SNN states (Voltage = 0)
+                reset_net(model)
 
-    final_miou = metric.compute().item()
-    print(f"\n=========================================")
-    print(f"🏆 Final Test mIoU: {final_miou:.4f}")
-    print(f"=========================================\n")
+        final_miou = metric.compute().item()
+        print(f"\n=========================================")
+        print(f"🏆 Final Test mIoU: {final_miou:.4f}")
+        print(f"=========================================\n")
 
 if __name__ == "__main__": # only run if execute python command.
     main() 
