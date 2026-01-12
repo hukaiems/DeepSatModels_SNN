dfa5a56e5ecaf1739aaa81960dbac7d6947746d0
hukaiems <nguoinhen0105@gmail.com>
2026-01-12 20:39:30 +0700

    fix comma err

--- train_and_eval/inference_s_tsvit.py
+++ train_and_eval/inference_s_tsvit.py
@@ -80,7 +80,7 @@ def get_args():
                         help="Plotting out which time step is the most importance for the accuracy of each class.")
     parser.add_argument('--temporal_importance_class', type=int, default=9,
                         help="This is the class number you want to plot the temporal importance experiment.")
-    parser.add_argument('--analyze_cloud', type=str, default=None, choices=['scan', 'compare']
+    parser.add_argument('--analyze_cloud', type=str, default=None, choices=['scan', 'compare'],
                         help=" Analyzing the cloud cover picture and plot out the prediction")
 
     return parser.parse_args()
